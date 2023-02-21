082233e75a476f4c9368ca1083fb466307c41eb0
Hislop, Ben <bhislop@thejrtagency.com>
2023-02-20 18:31:01 -0700

    started about page.

---
+++ src/components/HeadingWithCopy.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="copy-body">
+    <p class="bold">{{ heading }}</p>
+    <p>{{ bodyCopy }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["heading", "bodyCopy"],
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/AboutPage.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import Heading from "../components/Heading.vue"
+  import HeadingWithCopy from "../components/HeadingWithCopy.vue";
+  import TileContainer from "../components/TileContainer.vue";
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <Heading projectName="" heading="Yo, my name is Benjamin Hislop." subHeading="I'm a good human"/>
+      <HeadingWithCopy heading="Summary" bodyCopy="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse." />
+
+      <br>
+      <br>
+      <br>
+      <h1 class="blue heading-01">resume body todo</h1>
+      <br>
+      <br>
+      <br>
+
+      <HeadingWithCopy heading="Thing's I'm Good At" bodyCopy="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse." />
+      <HeadingWithCopy heading="Fun facts" bodyCopy="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse." />
+      <TileContainer heading="More Cool Things." :cardCount="extraCardTemp" />
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+  components: { Heading, TileContainer, HeadingWithCopy },
+  data() {
+      return {
+        extraCardTemp:3
+      }
+    }  
+  }
+</script>
