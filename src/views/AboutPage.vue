a9dec591ff27468a67f46fa0a01691c75a6cefb9
Hislop, Ben <bhislop@thejrtagency.com>
2023-02-28 09:08:20 -0700

    turned off form until I figure it out

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -14,28 +14,6 @@
       <br>
       <br>
       <h1 class="blue heading-01">resume body todo</h1>
-      
-      <p class="bold">netlify form test</p>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
       <br>
       <br>
       <br>
