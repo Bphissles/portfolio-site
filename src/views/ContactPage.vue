4a83af0d085d98d70ab8c5dc5be87471070ac645
Hislop, Ben <bhislop@thejrtagency.com>
2023-03-01 13:12:41 -0700

    the new page did not add in the "-am" param

---
+++ src/views/ContactPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import Heading from "@/components/Heading.vue"
+  import HeadingWithCopy from "@/components/HeadingWithCopy.vue";
+  import TileContainer from "@/components/TileContainer.vue";
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <Heading projectName="" heading="Yo, my name is Benjamin Hislop." subHeading="Let's be friends"/>
+      <HeadingWithCopy heading="Summary" bodyCopy="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse." />
+
+      <TileContainer heading="More Cool Things." :cardCount="extraCardTemp" />
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+  components: { Heading, TileContainer, HeadingWithCopy },
+  data() {
+      return {
+        extraCardTemp:3
+      }
+    }  
+  }
+</script>
