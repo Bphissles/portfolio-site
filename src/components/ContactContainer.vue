cf4bba6687eda2a31625efae740f3a0feeafa498
Hislop, Ben <bhislop@thejrtagency.com>
2023-03-01 20:12:26 -0700

    tidied up, added social links

---
+++ src/components/ContactContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3 class="heading-03 underline" v-for="data in sampleData">
+      <a :href="data.link" target="_blank"><i :class="data.icon"></i> {{ data.title }}</a>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contact-container",
+  data() {
+    return {
+      sampleData: [
+        {
+          icon: 'fa-brands fa-linkedin',
+          title: 'LinkedIn',
+          link: 'https://www.linkedin.com/in/benjaminhislop/'
+        },
+        
+        {
+          icon: 'fa-brands fa-github',
+          title: 'Github',
+          link: 'https://github.com/Bphissles'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+    <div class="top-inputs">
+      <p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+      </p>
+      <p>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"/>
+      </p>
+    </div>
+    <p>
+      <label for="message">Message:</label>
+      <textarea name="message" id="message" rows="6"></textarea>
+    </p>
+    <p>
+      <button>Send<i class="arrow"></i></button>
+    </p>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: "contact-form",
+  }
+</script>
+
+<style lang="scss" scoped>
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+
+  .arrow {
+    margin-left: 4px;
+    width: 12px;
+    height: 12px;
+  }
+
+  .top-inputs {
+    display: flex;
+    flex-direction: column;
+    > p {
+      flex: 0 0 48%
+    }
+    @media (min-width: 640px) {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+</style>
